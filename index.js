58cc9e12db5c512674d20da03c62f79feef20300
Sina Moradian <sinacsharp@gmail.com>
2022-08-28 21:48:17 +0430

    Add Search functionality and error handling

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const descriptionDOM = document.querySelector(".description");
 const humidityDOM = document.querySelector(".humidity");
 const weatherIconDOM = document.querySelector(".weather-icon");
 
+//set default weather info
+document.addEventListener("DOMContentLoaded", () => weather.fetchWeather("Tehran"));
+
 let weather = {
     apiKey: "673885386a9f5cdfefe931042c4d1129",
     fetchWeather: (city) => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather.apiKey}&units=metric`)
             .then(response => response.json())
-            .then(data => weather.displayWeather(data));
+            .then(data => weather.displayWeather(data))
+            .catch(() => {
+                alert("The entered city was not found!");
+                searchInput.value = "";
+            });
     },
     displayWeather: (data) => {
         const city = data.name;
@@ -25,7 +32,25 @@ let weather = {
         descriptionDOM.innerText = description;
         humidityDOM.innerText = `Humidity: ${humidity}%`;
         windDOM.innerText = `Wind speed: ${speed} km/h`;
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${city}')`;
+    },
+    search: () => {
+        if (searchInput.value != null) {
+            weather.fetchWeather(searchInput.value);
+        } else {
+            alert("Please enter a city name!");
+        }
     }
 }
 
+//search button
+searchBtn.addEventListener("click", weather.search);
+
+//ENTER key press
+searchInput.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
+        weather.search(searchInput.value);
+    }
+})
 
